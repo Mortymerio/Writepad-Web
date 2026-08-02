dd3587f8d7c7f0a7363977dcae25ba69d073c1a7
Mortymerio <mortymerio@gmail.com>
2026-08-02 08:23:07 -0300

    Updates

--- assets/PeasWiki-DcaQZPOI.js
+++ assets/PeasWiki-DcaQZPOI.js
@@ -1,4 +1,4 @@
-import{t as e}from"./index-fjlEUsrY.js";var t=`# LinPEAS - Linux Privilege Escalation Awesome Script
+import{t as e}from"./index-k8u9siuS.js";var t=`# LinPEAS - Linux Privilege Escalation Awesome Script
 
 LinPEAS es un script extremadamente completo que busca posibles rutas de escalada de privilegios en sistemas Linux/Unix. Busca configuraciones incorrectas, archivos con SUID, contraseñas en texto claro, y vulnerabilidades conocidas.
 
